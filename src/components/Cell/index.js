fee726f618d86e516a141f1d6834a78a00d6fd78
krenvp <karenperarnau@gmail.com>
2023-06-30 14:53:45 -0400

    Cell factory created

--- src/components/Cell/index.js
+++ src/components/Cell/index.js
@@ -1,10 +1,22 @@
+import { fillGameCanvas } from "@utils/globals";
+
 class Cell {
-  constructor() {
+  constructor(position) {
+    this.position = position;
     this.size = {
       width: 20,
       height: 20,
     };
     this.color = "#233311";
+  }
+
+  paint() {
+    const x = this.position.x;
+    const y = this.position.y;
+    const { width, height } = this.size;
+
+    const fill = { color: this.color, x, y, width, height };
+    fillGameCanvas(fill);
   }
 }
 
