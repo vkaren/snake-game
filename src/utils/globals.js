13f2aa173d60466b9aca9d4a77764b58af9de1fb
krenvp <karenperarnau@gmail.com>
2023-06-30 14:52:26 -0400

    Adding new global function, fill and draw canvas

--- src/utils/globals.js
+++ src/utils/globals.js
@@ -1,6 +1,7 @@
 import Game from "@pages/Game";
 
 const app = document.querySelector("#app");
+const gameCanvas = document.querySelector(".game-canvas");
 
 function playGame() {
   const gamePage = new Game();
@@ -8,4 +9,13 @@ function playGame() {
   app.replaceChildren(gamePage.render());
 }
 
-export { app, playGame };
+function fillGameCanvas({ color, x, y, width, height }) {
+  gameCanvas.fillStyle = color;
+  gameCanvas.fillRect(x, y, width, height);
+}
+
+function drawImgGameCanvas({ image, x, y, width, height }) {
+  gameCanvas.drawImage(image, x, y, width, height);
+}
+
+export { app, gameCanvas, playGame, fillGameCanvas, drawImgGameCanvas };
