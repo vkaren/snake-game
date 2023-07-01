ddf99ad4576c00c0da01fd5a9f429c2cf8e18113
krenvp <karenperarnau@gmail.com>
2023-07-01 18:34:57 -0400

    Adding new global functions and variables

--- src/utils/globals.js
+++ src/utils/globals.js
@@ -1,15 +1,26 @@
 import Game from "@pages/Game";
 
 const app = document.querySelector("#app");
+let canvasElem;
 let gameCanvas;
+let score;
+let highScore;
+let gameMsg;
 
 function playGame() {
-  const gamePage = new Game();
+  const gamePage = new Game({ speed: 200 });
 
   app.replaceChildren(gamePage.render());
 
-  let canvasElem = document.querySelector(".game-canvas");
+  // Getting the canvas and other elements after rendering the game
+  canvasElem = document.querySelector(".game-canvas");
   gameCanvas = canvasElem.getContext("2d");
+  score = document.querySelector(".game-score");
+  gameMsg = document.querySelector(".game-msg");
+  highScore = document.querySelector(".highScore-span");
+
+  // Creating the snake and the apple in canvas
+  gamePage.play();
 }
 
 function fillGameCanvas({ color, x, y, width, height }) {
@@ -21,4 +32,35 @@ function drawImgGameCanvas({ image, x, y, width, height }) {
   gameCanvas.drawImage(image, x, y, width, height);
 }
 
-export { app, gameCanvas, playGame, fillGameCanvas, drawImgGameCanvas };
+function clearGameCanvas() {
+  gameCanvas.clearRect(0, 0, canvasElem.width, canvasElem.height);
+}
+
+function updateScore(val) {
+  score.textContent = val;
+}
+
+function updateHighScore(val) {
+  highScore.textContent = val;
+}
+
+function toggleGameOverMsg() {
+  if (gameMsg.classList.contains("hidden")) {
+    gameMsg.classList.remove("hidden");
+  } else {
+    gameMsg.classList.add("hidden");
+  }
+}
+
+export {
+  app,
+  canvasElem,
+  gameCanvas,
+  playGame,
+  fillGameCanvas,
+  drawImgGameCanvas,
+  clearGameCanvas,
+  updateScore,
+  updateHighScore,
+  toggleGameOverMsg,
+};
